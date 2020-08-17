8802a1498207fefa8349af5dc36c6200bf2f8788
Colin Green <colin.green@knights.ucf.edu>
2020-08-17 14:47:02 -0400

    Created Layout component, burger builder container, ingredient component, propType validation, burger component

---
+++ src/components/Layout/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Auxilary from '../../hoc/Auxilary';
+import classes from './style.module.css';
+
+const layout = props => {
+	return (
+		<Auxilary>
+			<div>Toolbar, SideDrawer, Backdrop</div>
+			<main className={classes.Content}>{props.children}</main>
+		</Auxilary>
+	);
+};
+
+export default layout;
