dbf537adcd40275e865530c12d2a46524a88f4b8
Colin Green <colin.green@knights.ucf.edu>
2020-08-27 12:27:50 -0400

    Committing before changing folder structure for backup purposes

--- src/components/UI/Modal/index.js
+++ src/components/UI/Modal/index.js
@@ -1,23 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 import classes from './style.module.css';
 import Auxilary from '../../../hoc/Auxilary';
 import Backdrop from '../Backdrop';
 
-const modal = props => {
-	return (
-		<Auxilary>
-			<Backdrop show={props.show} clicked={props.modalClosed} />
-			<div
-				className={classes.Modal}
-				style={{
-					transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-					opacity: props.show ? '1' : '0',
-				}}
-			>
-				{props.children}
-			</div>
-		</Auxilary>
-	);
-};
+class Modal extends Component {
+	shouldComponentUpdate(nextProps, nextState) {
+		return nextProps.show !== this.props.show;
+	}
 
-export default modal;
+	componentWillUpdate() {
+		console.log('[Modal] will update');
+	}
+
+	render() {
+		return (
+			<Auxilary>
+				<Backdrop show={this.props.show} clicked={this.props.modalClosed} />
+				<div
+					className={classes.Modal}
+					style={{
+						transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
+						opacity: this.props.show ? '1' : '0',
+					}}
+				>
+					{this.props.children}
+				</div>
+			</Auxilary>
+		);
+	}
+}
+
+export default Modal;
